614092692c18893dd13fd4ec74c0bf8c1c4e80da
Mohamed Abdallah <MohamedMAbdallah14@gmail.com>
2019-07-17 16:41:03 +0200

    Initial Commit

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+    @IBOutlet weak var tweetLabel: UILabel!
+    @IBOutlet weak var tweetSentimentLabel: UILabel!
+    @IBOutlet weak var tweetCounrLabel: UILabel!
+    @IBOutlet weak var tweetProgressView: UIProgressView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func predictPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextTweet(_ sender: UIButton) {
+    }
+    
+    @IBAction func previousTweet(_ sender: UIButton) {
+    }
+    
 }
 
